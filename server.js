39a1ee286ebfbfb0eb09b09747578095d1839880
Ken Pierce <kpierce8@gmail.com>
2015-08-14 01:09:54 +0000

    testing cloud9 edits

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express'),
-	// config = require('./server/configure'),
+	config = require('./server/configure'),
 	app = express();
 
 app.set('port',process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
+
+app = config(app);
 
 var server = app.listen(app.get('port'), function() {
 	console.log('Serverup: http://localhost:' + app.get('port'));
